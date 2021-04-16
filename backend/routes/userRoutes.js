3c8cc62e9a4e71540b8f76e9ac2541623d0d4672
icharles2 <iacharles257@gmail.com>
2021-04-16 14:23:35 -0500

    added mongoose and user signup/login

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {
+	authUser,
+	getUserProfile,
+	registerUser,
+} from '../controllers/userController';
+import { protect } from '../middleware/authMiddleware';
+const router = express.Router();
+
+router.route('/').post(registerUser);
+router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
+
+export default router;
