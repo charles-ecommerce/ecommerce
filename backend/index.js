799366fb24dd87e97ab50d00302e612f28a9cda0
icharles2 <iacharles257@gmail.com>
2021-04-19 22:52:27 -0500

    added order routes to index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import { json, urlencoded } from 'body-parser';
 import { connectDB } from './config/db';
 import productRoutes from './routes/productRoutes';
 import userRoutes from './routes/userRoutes';
+import orderRoutes from './routes/orderRoutes';
 import { notFound, errorHandler } from './middleware/errorMiddleware';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -21,6 +22,7 @@ app.use(morgan('dev'));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
