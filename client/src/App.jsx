4c78921a41999e9a716d4e5eab7a993f8930e066
icharles2 <iacharles257@gmail.com>
2021-04-19 22:25:16 -0500

    + redux & cart,payment,placeorder,shipping/screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,15 @@ import { Container } from 'react-bootstrap';
 import { Route } from 'react-router';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const App = () => {
 	return (
@@ -14,9 +19,14 @@ const App = () => {
 			<Header />
 			<main className="py-3">
 				<Container>
+					<Route path="/shipping" component={ShippingScreen} />
+					<Route path="/payment" component={PaymentScreen} />
+					<Route path="/placeorder" component={PlaceOrderScreen} />
 					<Route path="/login" component={LoginScreen} />
 					<Route path="/register" component={RegisterScreen} />
+					<Route path="/profile" component={ProfileScreen} />
 					<Route path="/product/:id" component={ProductScreen} />
+					<Route path="/cart/:id?" component={CartScreen} />
 					<Route path="/" component={HomeScreen} exact />
 				</Container>
 			</main>
